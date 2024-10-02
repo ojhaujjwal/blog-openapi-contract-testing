5651323243cfb3ed31545246cbaefc1e00410c4d
Ujjwal Ojha <bring2uo@gmail.com>
2024-10-02 19:46:06 +1000

    Update base URI

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  base: '/blog-openapi-contract-testing',
   integrations: [
     tailwind({
       applyBaseStyles: false,
